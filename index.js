b11389fcf3bf406a8a2e3f207828cc8925cce40f
h1e2ll3o <60768888+h1e2ll3o@users.noreply.github.com>
2023-10-31 17:03:17 +0900

    Update index.js

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ setTimeout(function() {
   console.log("oneTime");
 }, 5000);
 
-self.scrollState = false;
-if (!self.scrollState) {
-  self.mainElem.classList.add("running");
-}
-self.scrollState = setTimeout(function() {
-  self.mainElem.classList.remove("running");
-}, 500);
+//self.scrollState = false;
+//if (!self.scrollState) {
+//  self.mainElem.classList.add("running");
+//}
+//self.scrollState = setTimeout(function() {
+//  self.mainElem.classList.remove("running");
+//}, 500);
