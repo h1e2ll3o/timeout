660f3d6dec6655fd3adc6f43dcc0b90f414bb3da
h1e2ll3o <60768888+h1e2ll3o@users.noreply.github.com>
2023-11-01 13:00:38 +0900

    Update and rename index.html to index.js

---
+++ index.js
@@ -0,0 +1,11 @@
+
+	const xhr = new XMLHttpRequest();
+	const method = "GET";
+	const url = "http://server.ocp412.lds.com";
+	
+	// 요청을 초기화 합니다.
+	xhr.open(method, url);
+	
+	// 서버에 요청을 보냅니다.
+	xhr.send();
+
